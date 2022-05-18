4cabf97a24dca6f9f16eaaaea8759d4a63dd4de0
hussain7482 <tousifh111@gmail.com>
2022-05-18 13:45:19 +0530

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+  validates :description, presence: true
+  validates :start_time, presence: true
+  validates :title, length: {in: 3..10}
+ 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+get "/list",to:"event#list"
+get "/search/:query",to:"event#search"
 
   # Defines the root path route ("/")
   # root "articles#index"
